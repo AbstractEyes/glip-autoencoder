813179e7e58e28b289cfc6499ae2d6c8e5d2be2c
AbstractPhil <philcoelho@live.com>
2026-02-26 08:23:12 -0800

    emptied inits

--- geolip/core/__init__.py
+++
@@ -1,3 +0,0 @@
-from .ksimplex_linear import KSimplexLinear
-from .topology import CantorTopology
-from .cayley_menger import CayleyMengerValidator
--- geolip/scales/__init__.py
+++
@@ -1,5 +0,0 @@
-from .patch import Patchifier
-from .state import TopologicalState
-from .patchwork import Patchwork, PatchworkOutput
-from .chunk import Chunk, ChunkOutput
-from .sector import Sector
